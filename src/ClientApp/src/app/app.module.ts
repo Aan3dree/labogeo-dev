37f3820b15a8ea2326c81752e5a191016c013588
André Luiz <an3dree@gmail.com>
2022-02-23 09:09:37 -0300

    Path for new pages

--- src/ClientApp/src/app/app.module.ts
+++ src/ClientApp/src/app/app.module.ts
@@ -9,6 +9,9 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { MembrosComponent } from './membros/membros.component';
+import { ProjetosComponent } from './projetos/projetos.component';
+import { ContatoComponent } from './contato/contato.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +19,10 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     NavMenuComponent,
     HomeComponent,
     CounterComponent,
-    FetchDataComponent
+    FetchDataComponent,
+    MembrosComponent,
+    ProjetosComponent,
+    ContatoComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -26,6 +32,9 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
+      { path: 'membros', component: MembrosComponent },
+      { path: 'projetos', component: ProjetosComponent},
+      { path: 'contato', component: ContatoComponent}
     ])
   ],
   providers: [],
